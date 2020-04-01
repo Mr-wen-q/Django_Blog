e6013f29422ccd771908a34936ec32b5c53541cf
wenq <wenq0818@163.com>
2020-04-01 18:13:43 +0800

    第二次提交

---
+++ my_blog/article/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path
+
+# Djiango2.0必须加app_name
+app_name = 'article'
+
+urlpatterns = [
+    # path函数将url映射到视图, name参数对应html中调用， views对应函数调用
+    path('article-list/', views.article_list, name='article_list'),
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    path('article-create/', views.article_create, name='article_create'),
+]
